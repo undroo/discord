644d5b338327e18652355c71c5f1525ced1dfb95
Andrew <katzensprung1@gmail.com>
2020-03-31 23:25:07 +1100

    roll dice included

--- bot.py
+++ bot.py
@@ -23,12 +23,21 @@ async def on_ready():
 @bot.command(name='activate', help='use at your own peril')
 async def activate(ctx):
     #basically covers if the author is the bot and avoids recursively sending itself a message
-    response = "YOU JUST ACTIVATED MY TRAP CARD"
+    response = "YOU JUST ACTIVATED MY TRAP CARD\nhttps://tenor.com/view/yugioh-yami-yugi-atem-draws-gif-5742435"
     await ctx.send(response)
 
-@bot.command(name='roll', help='Simulates rolling dice.')
+@bot.command(name='roll', help='number of dice, number of sides')
+@commands.has_role('admin')
 async def roll(ctx, number_of_dice: int, number_of_sides: int):
     out = ""
-    await ctx.send(', '.join(out))
+    for i in range(number_of_dice):
+        number_gen = random.randint(1,number_of_sides)
+        out = out + str(number_gen) + " "
+    await ctx.send(out)
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send('You do not have the correct role for this command.')
 
 bot.run(TOKEN)
